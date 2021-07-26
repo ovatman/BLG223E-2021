67db22018babba787a08bd0d1306898cf20e0070
Tolga Ovatman <ovatman@itu.edu.tr>
2021-07-26 14:04:22 +0000

    incomplete DoubleLinkedList

--- L05/src/main.cpp
+++ L05/src/main.cpp
@@ -4,10 +4,16 @@
 
 using namespace std;
 
+void single_linked_list();
+
 int main(int argc, char *argv[]){
 
+    //single_linked_list();
+    
+    return EXIT_SUCCESS;
+}
 
-
+void single_linked_list(){
     SingleLinkedList* s=new SingleLinkedList();
 
     s->add_node(5);
@@ -16,15 +22,18 @@ int main(int argc, char *argv[]){
     s->add_node(8);
 
     s->print_list();
-    s->clear_list();
-    
-    s->add_node(10);
-    s->add_node(8);
+    //s->clear_list();
+    if(s->contains(10))
+        cout<<"s contains 10"<<endl;
+    s->remove_node(5);
+    s->remove_node(1);
+    if(!s->contains(1))
+        cout<<"s does not contain 1"<<endl;
+    s->remove_node(10);
+    s->remove_node(8);
 
     s->print_list();
 
     delete s;
 
-    return EXIT_SUCCESS;
 }
-
